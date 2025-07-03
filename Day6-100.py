4636dc1c7cbfd2b1d0736daf89d098329c1d64e8
Alexandredecarvalhopaiva <alexandredecarvalhopaiva@gmail.com>
2025-07-02 22:54:36 -0300

    Vencendo o desafio do Reeborg's World - Hurdle 1

---
+++ Day6-100.py
@@ -0,0 +1,22 @@
+#Aprendendo sobre funções 
+#Disponivel no link: https://reeborg.ca/reeborg.html?lang=en&mode=python&menu=worlds%2Fmenus%2Freeborg_intro_en.json&name=Hurdle%202&url=worlds%2Ftutorial_en%2Fhurdle2.json
+#Selecione o mapa: Hurdle 1 
+
+def turn_right():
+    turn_left()
+    turn_left()
+    turn_left()
+def turn_around():
+    turn_left()
+    turn_left()
+
+for i  in range(5):
+    move()
+    turn_left()
+    move()
+    turn_right()
+    move()
+    turn_right()
+    move()
+    turn_left()
+done()
