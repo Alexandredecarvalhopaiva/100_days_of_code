4b6fe9506792f1752bd381961286c090dbcf3bf0
Alexandredecarvalhopaiva <alexandredecarvalhopaiva@gmail.com>
2025-07-03 23:36:39 -0300

    Concluindo o desafio 2 e 3 do Reeborg's Word

--- Day6-100.py
+++ Day6-100.py
@@ -20,3 +20,52 @@ for i  in range(5):
     move()
     turn_left()
 done()
+
+# Desafio 2 Hurdle 2 https://reeborg.ca/reeborg.html?lang=en&mode=python&menu=worlds%2Fmenus%2Freeborg_intro_en.json&name=Hurdle%202&url=worlds%2Ftutorial_en%2Fhurdle2.json
+
+def turn_right():
+    turn_left()
+    turn_left()
+    turn_left()
+def turn_around():
+    turn_left()
+    turn_left()
+def running():
+    move()
+    turn_left()
+    move()
+    turn_right()
+    move()
+    turn_right()
+    move()
+    turn_left()
+ 
+while at_goal() != True:
+    running()
+
+# Desafio 3 - Hurdle 3:
+
+def turn_right():
+    turn_left()
+    turn_left()
+    turn_left()
+def turn_around():
+    turn_left()
+    turn_left()
+def running():
+   # move()
+    turn_left()
+    move()
+    turn_right()
+    move()
+    turn_right()
+    move()
+    turn_left()
+
+while at_goal() == False:
+    if front_is_clear() == True:
+        move()
+    if wall_in_front() == True:
+        running()
+
+     
