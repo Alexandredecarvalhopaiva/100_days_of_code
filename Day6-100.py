4a01bf9de2d2a66aaef6acc372a4f5cd379ab00c
Alexandredecarvalhopaiva <alexandredecarvalhopaiva@gmail.com>
2025-07-05 13:00:51 -0300

    Iniciando o desafio 4: Hurdle 3

--- Day6-100.py
+++ Day6-100.py
@@ -68,4 +68,28 @@ while at_goal() == False:
     if wall_in_front() == True:
         running()
 
+# Desafio 4 - Hurdle 4:
+def turn_right():
+    turn_left()
+    turn_left()
+    turn_left()
+def turn_around():
+    turn_left()
+    turn_left()
+def running():
+   # move()
+    turn_left()
+    move()
+    turn_right()
+    move()
+    turn_right()
+    move()
+    turn_left()
+
+while at_goal() == False:
+    if front_is_clear() == True:
+        move()
+    elif wall_in_front() == True:
+        running()
+
      
