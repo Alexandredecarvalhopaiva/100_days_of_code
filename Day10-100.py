8d8762585f4c1c2cb31e076f35c2b339596abbcd
Alexandredecarvalhopaiva <alexandredecarvalhopaiva@gmail.com>
2025-11-26 05:44:36 -0300

    Adicionando Docstrings na função

--- Day10-100.py
+++ Day10-100.py
@@ -1,4 +1,6 @@
 def ano_bissexto(ano):
+    """Digite o ano e eu te direi se ele é um ano bissexto"""
+    
     if (ano % 4 == 0 and ano % 100 != 0) or (ano % 400 == 0):
         print(f"O ano {ano} é bissexto.")
     else:
